3353bad93068dbf772aae0d3818689a7cb3c1787
Petter B <petter.bergstrom@protonmail.com>
2023-05-21 09:38:35 +0200

    Rename main class

--- src/main/java/se/pbt/iths/shapesfx/ShapesApplication.java
+++ src/main/java/se/pbt/iths/shapesfx/ShapesApplication.java
@@ -7,8 +7,7 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
-    @Override
+public class ShapesApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
