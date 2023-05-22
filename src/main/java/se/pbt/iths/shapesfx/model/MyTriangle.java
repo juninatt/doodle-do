bbda54df9cabfab660bce35da77d6589f1dfe08a
Petter B <petter.bergstrom@protonmail.com>
2023-05-22 16:25:46 +0200

    Refactor MyTriangle.java
    - add field
    - change constructor signature
    - change color field from 'Color' to 'Paint' class
    - add getter for paint and new field

--- src/main/java/se/pbt/iths/shapesfx/model/MyTriangle.java
+++ src/main/java/se/pbt/iths/shapesfx/model/MyTriangle.java
@@ -1,24 +1,27 @@
 package se.pbt.iths.shapesfx.model;
 
-import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
 import javafx.scene.shape.Polygon;
 
 public class MyTriangle extends Polygon {
-    private Color color;
+    private Paint paint;
+    double sideLength;
     private boolean selected;
 
-    public MyTriangle(double x1, double y1, double x2, double y2, double x3, double y3, Color color) {
-        super(x1, y1, x2, y2, x3, y3);
-        this.color = color;
-        this.selected = false;
-        setFill(color);
+    public MyTriangle(double sideLength, Paint paint) {
+        super();
+        this.sideLength = sideLength;
+        this.paint = paint;
     }
 
-    // Additional methods specific to MyTriangle class
 
-    public void setColor(Color color) {
-        this.color = color;
-        setFill(color);
+    public void setPaint(Paint paint) {
+        this.paint = paint;
+        setFill(paint);
+    }
+
+    public Paint getPaint() {
+        return paint;
     }
 
     public boolean isSelected() {
@@ -27,6 +30,9 @@ public class MyTriangle extends Polygon {
 
     public void setSelected(boolean selected) {
         this.selected = selected;
-        // Update appearance or perform any other necessary actions
+    }
+
+    public double getSideLength() {
+        return sideLength;
     }
 }
