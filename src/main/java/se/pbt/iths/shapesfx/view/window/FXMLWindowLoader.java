87224fe692d7f1bcb022f9aeeedf78ea515480ac
Petter B <petter.bergstrom@protonmail.com>
2023-05-23 21:48:26 +0200

    Convert FXMLWindowLoader.java to a record

--- src/main/java/se/pbt/iths/shapesfx/view/window/FXMLWindowLoader.java
+++ src/main/java/se/pbt/iths/shapesfx/view/window/FXMLWindowLoader.java
@@ -9,18 +9,9 @@ import se.pbt.iths.shapesfx.ShapesApplication;
 
 import java.io.IOException;
 
-public class FXMLWindowLoader {
-    private final String title;
-    private final String fxmlFile;
-    private final Modality modality;
+public record FXMLWindowLoader(Stage stage, String title, String fxmlFile, Modality modality) {
 
-    public FXMLWindowLoader(String title, String fxmlFile, Modality modality) {
-        this.title = title;
-        this.fxmlFile = fxmlFile;
-        this.modality = modality;
-    }
-
-    public void loadWindow(){
+    public void loadWindow() {
         try {
             FXMLLoader fxmlLoader = new FXMLLoader(ShapesApplication.class.getResource(fxmlFile));
             Parent root = fxmlLoader.load();
@@ -32,7 +23,6 @@ public class FXMLWindowLoader {
     }
 
     private void createModalStage(Parent parent) {
-        Stage stage = new Stage();
         stage.initModality(modality);
         stage.setTitle(title);
         stage.setScene(new Scene(parent));
