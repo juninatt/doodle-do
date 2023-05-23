1e18ac07e1ccf32c597d86d460ccb4bb787b7662
Petter B <petter.bergstrom@protonmail.com>
2023-05-23 17:08:04 +0200

    Add SelectedShape.java
    - class to hold object for easy access across API

---
+++ src/main/java/se/pbt/iths/shapesfx/modelmanagement/SelectedShape.java
@@ -0,0 +1,27 @@
+package se.pbt.iths.shapesfx.modelmanagement;
+
+import javafx.scene.shape.Shape;
+
+public class SelectedShape {
+    private static SelectedShape instance;
+    private Shape selectedShape;
+
+    private SelectedShape() {
+        // Private constructor to prevent direct instantiation
+    }
+
+    public static SelectedShape getInstance() {
+        if (instance == null) {
+            instance = new SelectedShape();
+        }
+        return instance;
+    }
+
+    public void setSelectedShape(Shape shape) {
+        selectedShape = shape;
+    }
+
+    public Shape getSelectedShape() {
+        return selectedShape;
+    }
+}
