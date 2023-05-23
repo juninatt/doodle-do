b94a59d1c82a751e0abb2fe4b63f8d611b7ffc55
Petter B <petter.bergstrom@protonmail.com>
2023-05-23 10:55:54 +0200

    Refactor MySquare.java
    - change constructor signature
    - change color field from 'Color' to 'Paint' class
    - add getter for Paint field

--- src/main/java/se/pbt/iths/shapesfx/model/MySquare.java
+++ src/main/java/se/pbt/iths/shapesfx/model/MySquare.java
@@ -1,23 +1,27 @@
 package se.pbt.iths.shapesfx.model;
 
-import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
 import javafx.scene.shape.Rectangle;
 
 public class MySquare extends Rectangle {
-    private Color color;
+    private Paint paint;
     private boolean selected;
 
-    public MySquare(double x, double y, double size, Color color) {
-        super(x, y, size, size);
-        this.color = color;
+    public MySquare(double size, Paint paint) {
+        super(size, size);
+        this.paint = paint;
         this.selected = false;
-        setFill(color);
+        setFill(paint);
     }
 
 
-    public void setColor(Color color) {
-        this.color = color;
-        setFill(color);
+    public void setPaint(Paint paint) {
+        this.paint = paint;
+        setFill(paint);
+    }
+
+    public Paint getPaint() {
+        return paint;
     }
 
     public boolean isSelected() {
