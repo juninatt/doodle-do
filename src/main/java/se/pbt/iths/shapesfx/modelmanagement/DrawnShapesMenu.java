b8c04f99e8bc9bc2b3a9e6717df2ec8e509bdcd6
Petter B <petter.bergstrom@protonmail.com>
2023-08-01 22:28:10 +0200

    Make DrawnShapesMenu thread safe and unmodifiable
    - make class instance thread safe
    - make list of shapes unmodifiable
    - add check for duplicate names

--- src/main/java/se/pbt/iths/shapesfx/modelmanagement/DrawnShapesMenu.java
+++ src/main/java/se/pbt/iths/shapesfx/modelmanagement/DrawnShapesMenu.java
@@ -4,12 +4,7 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import se.pbt.iths.shapesfx.interfaces.ShapeProperties;
 
-// TODO: Make thread safe
-// TODO: Return unmodifiable list
-// TODO: Forbid duplicate shape names
 public class DrawnShapesMenu {
-
-    public static DrawnShapesMenu instance;
 
     ObservableList<ShapeProperties> storedShapes;
 
@@ -17,22 +12,29 @@ public class DrawnShapesMenu {
         storedShapes = FXCollections.observableArrayList();
     }
 
-    public static DrawnShapesMenu getInstance() {
-        if (instance == null) {
-            instance = new DrawnShapesMenu();
-        }
-        return instance;
+    private static final class InstanceHolder {
+        public static final DrawnShapesMenu instance = new DrawnShapesMenu();
     }
 
-    public void addShape(ShapeProperties shape) {
-        if (shape != null) {
-            storedShapes.add(shape);
+    public static DrawnShapesMenu getInstance() {
+        return InstanceHolder.instance;
+    }
+
+
+    public synchronized void addShape(ShapeProperties newShape) {
+        boolean nameExists = storedShapes.stream()
+                .anyMatch(storedShape -> storedShape.getName().equals(newShape.getName()));
+        if (newShape != null) {
+            if (nameExists) {
+                throw new IllegalArgumentException("Duplicate shape name is not allowed");
+            }
+            storedShapes.add(newShape);
         } else {
             throw new IllegalArgumentException("Shape cannot be null");
         }
     }
 
-    public void removeShape(ShapeProperties shape) {
+    public synchronized void removeShape(ShapeProperties shape) {
         if (shape != null) {
             storedShapes.remove(shape);
         } else {
@@ -40,7 +42,7 @@ public class DrawnShapesMenu {
         }
     }
 
-    public ObservableList<ShapeProperties> getSavedShapes() {
-        return storedShapes;
+    public synchronized ObservableList<ShapeProperties> getSavedShapes() {
+        return FXCollections.unmodifiableObservableList(storedShapes);
     }
 }
